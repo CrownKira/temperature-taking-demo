8c7c4d25205ce0b315e7f67e7b97ca3da44c452d
CrownKira <t.karwi@yahoo.com>
2021-01-08 10:47:08 +0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "registrations" }
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get "post/index"
+      post :auth, to: "authentication#create"
+      get "/auth" => "authentication#fetch"
+      # get "/temperatures", to: "temperatures#index"
+      # post "/temperature", to: "temperatures#create"
+      # get "/temperature", to: "temperatures#show"
+    end
+  end
+end
+
+# http://localhost:3000/api/v1/post/index
+# CREATE USER
+# http://localhost:3000/users
+# AUTHORISE USER
+# will receive a token
+# http://localhost:3000/api/v1/auth
