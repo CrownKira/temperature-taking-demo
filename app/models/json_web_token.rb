b9e281e768a57b82f23e00248b4df974d4a3ce82
CrownKira <t.karwi@yahoo.com>
2021-01-08 14:46:53 +0800

    Fix secret_key_base issues

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,9 +1,9 @@
 class JsonWebToken
   def self.encode(payload)
     expiration = 2.weeks.from_now.to_i
-    JWT.encode payload.merge(exp: expiration), "16fc021a1a7a883b6523432fb6aace7507f1b09595d3eaa2bf1c771be86dc8dbf84552e38cff2bc59f82813e0a8a3fa5ccc660dd380136244d5f0e3c4bf2b5b2"
+    JWT.encode payload.merge(exp: expiration), Rails.application.credentials.fetch(:secret_key_base)
   end
   def self.decode(token)
-    JWT.decode(token, "16fc021a1a7a883b6523432fb6aace7507f1b09595d3eaa2bf1c771be86dc8dbf84552e38cff2bc59f82813e0a8a3fa5ccc660dd380136244d5f0e3c4bf2b5b2").first
+    JWT.decode(token, Rails.application.credentials.fetch(:secret_key_base)).first
   end
 end
